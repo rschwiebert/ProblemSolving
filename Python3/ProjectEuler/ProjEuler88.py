08fd4a56e653d0e18b368d481661f59414bf316f
rschwiebert <ryanschwiebert@ryans-mbp.myfiosgateway.com>
2020-03-29 19:47:20 -0400

    Solution for 88

--- Python3/ProjectEuler/ProjEuler88.py
+++ Python3/ProjectEuler/ProjEuler88.py
@@ -1,64 +1,48 @@
 #!/usr/bin/python
 __author__ = 'ryan'
+
 # https://projecteuler.net/problem=88
 # What is the sum of all the minimal product-sum numbers for 2<=k<=12000?
 
-# method below is too slow. You basically need to check every partition of every factorization of every number
-# until you hit the right answer.
+# Observation: for $n$ length tuples, (1,1,1,...,2,n) is always a solution. Therefore we are limited to sum-numbers
+# less or equal to 24000. For each n between 2 and 12000, we can generate all nondecreasing lists with product n,
+# and then append 1's until its sum matches its product (which we know is n).  This generates all the relevant
+# sum-product numbers for the problem.
 
-from collections import deque, namedtuple
-from copy import copy
-
-Item = namedtuple('Item', ['sum', 'prod', 'list'])
-
-
-def ndec_list_incrementer(ndec):
-    """
-    Walks a nondecreasing list, yielding nondecreasing lists which have sum
-    one greater than the input list.
-    :param ndec_list: a nondecreasing list of positive integers
-    """
-    fake_list = list(ndec.list) + [ndec.list[-1] + 1]
-    i = 0
-    while i < len(ndec.list):
-        if fake_list[i] == fake_list[i + 1]:
-            i += 1
-            continue
-        new_list = list(ndec.list)
-        new_sum = ndec.sum + 1
-        new_prod = ndec.prod / new_list[i] * (new_list[i] + 1)
-        new_list[i] += 1
-        new_list = tuple(new_list)
-        new_item = Item(sum=new_sum, prod=new_prod, list=new_list)
-        yield new_item
-        i += 1
-
-
-def ndec_list_generator(N):
-    """
-    Generates a sequence level-traversing a tree of nondecreasing sequences
-    :param N: the length of the list of integers 
-    """
-    Q = deque()
-    seen = set()
-    first = Item(sum=N, prod=1, list=(1,) * N)
-    Q.appendleft(first)
-    while Q:
-        cur = Q.pop()
-        yield cur
-        if cur not in seen:
-            seen.add(cur)
-            Q.extendleft(ndec_list_incrementer(cur))
+from copy import deepcopy
+from bisect import insort
 
 if __name__ == '__main__':
-    results = set()
-    for i in range(2, 12001):
-        ndec_iter = ndec_list_generator(i)
-        for item in ndec_iter:
-            if item.sum != item.prod:
+    factorizations = [[[i, ]] for i in range(24001)]
+    results = [50000] * 12001
+
+    for i in range(2, 24001):
+
+        # While we're here, check what sum-product numbers we have developeds so far
+        for fzn in factorizations[i]:
+            tot = sum(fzn)
+            length = len(fzn)
+            diff = i - tot
+            length += diff
+            if length > 12000:
                 continue
-            else:
-                break
-        print('Found {} for {}.'.format(item, i))
-        results.add(item.sum)
-    print("The answer is: {}".format(sum(results)))
+            if i < results[length]:
+                results[length] = i
+
+        # Advance multiple by multiple filling out factorizations further down the list
+        j = 2
+        ind = i * j
+        while ind <= 24000:
+            copies = deepcopy(factorizations[i])
+            for cpy in copies:
+                insort(cpy, j)  # Keeping lists sorted aids de-duplication in the next loop
+                if cpy not in factorizations[ind]:
+                    factorizations[ind].append(cpy)
+
+            j += 1
+            ind = i * j
+
+    # Along the way, I discovered the entries of `results` are no necessarily nondecreasing!
+    deduped_results = set(results[2:])
+    answer = sum(deduped_results)
+    print('Answer: {}'.format(answer))
